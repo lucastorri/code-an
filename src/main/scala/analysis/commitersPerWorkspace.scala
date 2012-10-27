69ca4a3f24c114ad60d255fb39608a2f4824f8a3
Lucas Torri <lucastorri@gmail.com>
2012-10-26 23:03:33 -0200

    analyzer to check who is commiting to the workspaces

---
+++ src/main/scala/analysis/commitersPerWorkspace.scala
@@ -0,0 +1,23 @@
+package co.torri.dod.analysis
+
+import spark._
+import com.thoughtworks.dod._
+
+
+class CommitersPerWorkspaceAnalyzer extends Analyzer {
+    val desc = "Commiters / Workspace"
+
+    def apply(data: RepoData, sc: SparkContext) = {
+
+        val d = data.commits
+            .map(c => (c.author, c.files.flatMap(fc => fc.workspace)))
+            .flatMap { case (author, workspaces) => workspaces.map(ws => (author, ws)) }
+            .groupBy { case (author, ws) => ws }
+            .map { case (ws, authors) => (ws, authors.distinct.map(_._1)) }
+            .toArray
+            .sortBy { case (ws, commiters) => - commiters.size }
+            .map { case (ws, commiters) => List(ws, commiters.size, commiters.sorted.mkString(", ")) }
+
+        Result(Seq("workspace", "# commiters", "commiters"), d)
+    }
+}
