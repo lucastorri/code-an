0467789b503ba683660f6b2f80357b90e22107b5
Lucas Torri <lucastorri@gmail.com>
2012-10-30 16:23:00 -0200

    adding stories paired and not single played

--- src/main/scala/analysis/storiesPaired.scala
+++ src/main/scala/analysis/storiesPaired.scala
@@ -5,20 +5,31 @@ import com.thoughtworks.dod._
 
 
 class StoriesPairedAnalyzer extends Analyzer {
-    val desc = "Commiter Paired Stories per Project"
+    val desc = "Commiter Work Mode per Project"
 
     def apply(data: RepoData, sc: SparkContext) = {
-        val d = data.issues
+        val blinkFixedStories = data.issues
             .filter(issue => issue.project == "BLINK" && issue.status == "Fixed")
+
+        val pairedStories = blinkFixedStories
             .filter(issue => issue.devs.map(devs => devs.size).orElse(Some(0)).map(_ >= 2).get)
             .flatMap(issue => issue.devs.get.map(dev => (dev, issue)))
             .groupBy { case (dev, issue) => dev }
             .map { case (dev, stories) => (dev, stories.size) }
-            .toArray
-            .sortBy { case (dev, nstories) => - nstories }
-            .map { case (dev, stories) => List(dev, stories) }
 
-        Result(Seq("user", "# paired stories"), d)
+        val workedAlone = data.issues
+            .filter(issue => issue.devs.map(devs => devs.size).orElse(Some(0)).map(_ == 1).get)
+            .groupBy(issue => issue.devs.get.head)
+            .map { case (dev, issues) => (dev, issues.size) }
+
+        val d = pairedStories.cartesian(workedAlone)
+            .filter { case ((pairDev, nstoriesPaired), (singleDev, nstoriesAlone)) => pairDev == singleDev }
+            .map { case ((pairDev, nstoriesPaired), (singleDev, nstoriesAlone)) => (pairDev, nstoriesPaired, nstoriesAlone) }
+            .toArray
+            .sortBy { case (dev, nstoriesPaired, nstoriesAlone) => dev }
+            .map { case (dev, nstoriesPaired, nstoriesAlone) => List(dev, nstoriesPaired, nstoriesAlone) }
+
+        Result(Seq("user", "# paired stories", "# single played stories"), d)
     }
 
 }
