4de69463a4b47361c8258d728784583272f9957b
Lucas Torri <lucastorri@gmail.com>
2012-11-09 17:34:30 -0200

    ranking users based on modifications

--- src/main/scala/analysis/checkinsPerAuthor.scala
+++ src/main/scala/analysis/checkinsPerAuthor.scala
@@ -14,3 +14,23 @@ class CheckinsPerAuthorAnalyzer extends Analyzer {
                 .sortBy { case (author, count) => - count }
                 .map(_.productIterator.toList))
 }
+
+class ModificationPerAuthorAnalyzer extends Analyzer {
+
+    def apply(data: RepoData, sc: SparkContext) =
+        Result(Seq("author", "added", "removed"),
+            data.commits
+            .flatMap(c => c.files.map(f => (c.author, f.added, f.removed)))
+            .groupBy { case (author, added, removed) => author }
+            .map { case (author, changes) =>
+                val (added, removed) = changes
+                    .foldLeft((0,0)) { case ((totalAdded, totalRemoved), (author, added, removed)) =>
+                        (totalAdded + added, totalRemoved + removed)
+                    }
+                (author, added, removed)
+            }
+            .toArray
+            .sortBy { case (author, added, removed) => - (added + removed) }
+            .map(_.productIterator.toList))
+
+}
